85b40c49d8762d6bd8dca03d7e0d6c9da3cc1ac0
FarhanKurnia <itsfarhankurnia@.com>
2022-03-28 01:34:00 +0700

    Add Delete Method

--- resources/views/visitor/home.blade.php
+++ resources/views/visitor/home.blade.php
@@ -12,8 +12,8 @@
             <p class="lead text-center">Welcome to Citranet Data Centers</p>
         </div>
     </div>
-    <div class="row justify-content-center">
-        <div class="col-sm text-center mb-3">
+    <div class="row">
+        <div class="col text-center">
             <a href="check-in" class="btn btn-danger btn-lg btn-block">Check In</a>
             <a href="check-out" class="btn btn-danger btn-lg btn-block">Check Out</a>
         </div>
